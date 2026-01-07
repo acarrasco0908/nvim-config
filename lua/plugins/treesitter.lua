d0befdcedac3dc2ccf9f56b576d0e020e483b691
acarrasco0908 <acarrasco0908@gmail.com>
2026-01-07 13:22:35 -0600

    Disabled 'c' in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     opts = {
       ensure_installed = {
                 'bash',
-                'c',
+--                'c',
                 'cmake',
                 'diff',
                 'html',
@@ -24,6 +24,7 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
+        disable = {"c"},
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
