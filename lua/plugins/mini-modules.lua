308a691b1b5c92434c59a3cc1608098055db9d50
acarrasco0908 <acarrasco0908@gmail.com>
2026-01-20 22:55:56 -0600

    Combined mini modules

--- lua/plugins/mini-modules.lua
+++ lua/plugins/mini-modules.lua
@@ -1,8 +1,11 @@
 return {
     'nvim-mini/mini.nvim',
+    enabled = true,
     version = false,
     config = function()
+        local statusline = require('mini.statusline')
         local minipairs = require('mini.pairs')
+        statusline.setup { use_icons = true }
         minipairs.setup {}
     end
 }
